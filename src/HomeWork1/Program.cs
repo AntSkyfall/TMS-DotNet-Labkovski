f5a900e652857e0fcae5b64f84f1b329aea0e10e
AntSkyfall <70284961+AntSkyfall@users.noreply.github.com>
2020-09-09 01:20:04 +0300

    Handler for the entered day of the week

--- src/HomeWork1/Program.cs
+++ src/HomeWork1/Program.cs
@@ -6,8 +6,19 @@ namespace HomeWork1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.Write("Введи дату: "); // выводит в консоль текст для информативности
+            string input = Console.ReadLine(); // инициализация переменной оператором присваивания с помощью ввода в консоль
+            bool condition = DateTime.TryParse(input, out DateTime result); // проверяю на корректность введенных данных. результат - правда/ложь... 
+            // Инициализирую переменную буллевого типа c помощью оператора присваивания результатом метода Tryparse
+            if (condition) // если condition truе то работает блок if, если condition false то работает блок else 
+            {
+                Console.Write("Ввод корректный: " + result.DayOfWeek + " " + result.DayOfYear ); // вывожу в консоль текст и значения свойства DayOfWeek и DayOfYear
+            }
+            else
+            {
+                Console.Write("некорректный ввод: "); // вывожу тест о ошибке
+            }
         }
     }
-}
+}          
+           
