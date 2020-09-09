74e0046685d3e40bb37c4602de210dbd867fd4a9
Anton dqdw <antskyfall@gmail.com>
2020-09-09 19:54:16 +0300

    fix: change language

--- src/HomeWork1/Program.cs
+++ src/HomeWork1/Program.cs
@@ -6,17 +6,17 @@ namespace HomeWork1
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите дату: "); // выводит в консоль текст для информативности
+            Console.Write("Enter data: "); // выводит в консоль текст для информативности
             string input = Console.ReadLine(); // инициализация переменной оператором присваивания с помощью ввода в консоль
             bool condition = DateTime.TryParse(input, out DateTime result); // проверяю на корректность введенных данных. результат - правда/ложь... 
             // Инициализирую переменную буллевого типа c помощью оператора присваивания результатом метода Tryparse
             if (condition) // если condition truе то работает блок if, если condition false то работает блок else 
             {
-                Console.Write("Ввод корректный: " + result.DayOfWeek + " " + result.DayOfYear ); // вывожу в консоль текст и значения свойства DayOfWeek и DayOfYear
+                Console.Write("successful input: " + result.DayOfWeek + " " + result.DayOfYear ); // вывожу в консоль текст и значения свойства DayOfWeek и DayOfYear
             }
             else
             {
-                Console.Write("Некорректный ввод. "); // вывожу тест об ошибке
+                Console.Write("incorrect input. "); // вывожу тест об ошибке
             }
         }
     }
