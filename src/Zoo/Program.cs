ab427b2f6a214aba71766d6b08a106ac0000886d
AntSkyfall <70284961+AntSkyfall@users.noreply.github.com>
2020-10-09 02:44:31 +0300

    init: HomeWork4|5 Zoo

---
+++ src/Zoo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Zoo
+{
+    abstract class AnimalBase //Абстрактный класс тут
+    {
+        public string name;
+        public int age = 1;
+    }
+    class Cat : AnimalBase
+    {
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+    class british : Cat // Наследование
+    {
+        public void PublicProperties()
+        {
+            Console.WriteLine("PublicProperties");
+        }
+        private void PrivateProperties()// Была использована инкапсуляция
+        {
+            Console.WriteLine("not for the eyes");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            british Vasya = new british();
+            Vasya.age = 5;
+            Vasya.name = "Vasiliy";
+            Vasya.GetInfo();
+            Vasya.PublicProperties();
+
+
+
+
+        }
+    }
+}
