0ce67d369cf8447bdf908bb79854a1da2ad51705
AntSkyfall <70284961+AntSkyfall@users.noreply.github.com>
2020-10-16 00:36:36 +0300

    init: add solution HomeWork5

---
+++ src/HomeWork 5/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_5
+{
+    class Account
+    {
+        public delegate void AccountStateHandler(string message);
+
+        AccountStateHandler _del;
+
+        public void RegisterHandler(AccountStateHandler del)
+        {
+            _del = del;
+        }
+
+        public Account(int sum)
+        {
+            Sum = sum;
+        }
+        public int Sum { get; private set; }
+        public void Put(int sum)
+        {
+            Sum += sum;
+
+            if (_del != null)
+                _del($"It came to the account {sum}");
+        }
+
+        public void Witchdraw (int sum)
+        {
+            if (sum <= Sum)
+            {
+                Sum -= sum;
+
+                if (_del != null)
+                    _del($"Sum {sum} Withdrawn from the account");
+            }
+            else
+            {
+                if (_del != null)
+                    _del($"Not enough money in the account");
+            }
+        }
+    }
+}
---
+++ src/HomeWork 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace HomeWork_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(0);
+            account.RegisterHandler(new Account.AccountStateHandler(Show_message));
+
+            Console.WriteLine("Deposit amount: ");
+            int Deposit = int.Parse(Console.ReadLine());
+            account.Put(Deposit);
+            Console.WriteLine($"On account sum: { account.Sum}");
+
+            Console.WriteLine("Witchdraw: ");
+            int Witchdraw = int.Parse(Console.ReadLine());
+            account.Witchdraw(Witchdraw);
+            Console.WriteLine($"On account sum: { account.Sum}");
+            Console.ReadLine();
+        }
+        private static void Show_message(String message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
